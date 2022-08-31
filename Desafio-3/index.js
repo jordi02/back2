a02811bd53d16dd0444187b0e0a4d226effc1015
jordi <jordigimbernat02@gmail.com>
2022-08-30 23:09:34 -0300

    desafio-3

---
+++ Desafio-3/index.js
@@ -0,0 +1,27 @@
+const express = require("express")
+const fs = require("fs");
+
+
+const Contenedor = require("./main");
+const producto1 = {
+  title: "Tenedor",
+  price: 50.45,
+};
+
+const constructor = new Contenedor("./productos.txt");
+
+constructor.save(producto1);
+
+const app = express();
+
+app.get("/productos", (req, res) => {
+  res.send(constructor.getAll());
+});
+
+app.get("/productoRandom", (req, res) => {
+  res.send(constructor.getRandom());
+});
+
+const server = app.listen(8080, () => {
+  console.log("Server Encendido");
+});
